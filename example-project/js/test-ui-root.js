0324fad411efaa8932bc9e1891d6e84c5d5fe500
rafern <rafern@protonmail.com>
2023-01-25 10:42:32 +0000

    Fix cursor styling and add rounded corners in example project

--- example-project/js/test-ui-root.js
+++ example-project/js/test-ui-root.js
@@ -1,4 +1,4 @@
-import { Label, Margin, Column, Row, TextInput, TextButton, Alignment, FlexAlignment, ValidatedVariable, Background } from 'lazy-widgets';
+import { Label, Margin, Column, Row, TextInput, TextButton, Alignment, FlexAlignment, ValidatedVariable, Background, RoundedCorners } from 'lazy-widgets';
 import { WLRoot } from '../../dist/index.esm.js';
 /*global WL*/
 
@@ -9,40 +9,42 @@ WL.registerComponent('test-ui-root', {
     init() {
         const label = new Label('Hello world!');
         this.root = new WLRoot(this.object, this.material,
-            new Background(
-                new Margin(
-                    new Column()
-                    .add(label)
-                    .add(
-                        new TextInput(new ValidatedVariable('', null, value => {
-                            label.text = `Text input: ${value}`;
-                        }, false))
-                    )
-                    .add(
-                        new Row({
-                            multiContainerAlignment: {
-                                main: FlexAlignment.Center,
-                                cross: Alignment.Stretch
-                            },
-                        }).add([
-                            new TextButton('Button 1', () => label.text = 'Button 1 clicked!'),
-                            new TextButton('Button 2', () => label.text = 'Button 2 clicked!'),
+            new RoundedCorners(
+                new Background(
+                    new Margin(
+                        new Column().add([
+                            label,
+                            new TextInput(new ValidatedVariable('', null, value => {
+                                label.text = `Text input: ${value}`;
+                            }, false)),
+                            new Row({
+                                multiContainerAlignment: {
+                                    main: FlexAlignment.Center,
+                                    cross: Alignment.Stretch
+                                },
+                            }).add([
+                                new TextButton('Button 1', () => label.text = 'Button 1 clicked!'),
+                                new TextButton('Button 2', () => label.text = 'Button 2 clicked!'),
+                            ])
                         ])
-                    )
+                    ),
                 ),
             ),
         );
     },
     update(_dt) {
-        if(this.root)
+        if(this.root) {
             this.root.update();
+        }
     },
     onActivate() {
-        if(this.root)
+        if(this.root) {
             this.root.enabled = true;
+        }
     },
     onDeactivate() {
-        if(this.root)
+        if(this.root) {
             this.root.enabled = false;
+        }
     },
 });
